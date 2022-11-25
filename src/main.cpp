cb0bea0af6eeb79fe1f328dbb2d05a958fd29a2d
jspearus <jspearus@gmail.com>
2022-11-25 08:54:24 -0600

    blank template

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,10 @@
 #include <Arduino.h>
-#define LIGHT_1 5
-#define LIGHT_2 6
 
 String DataIn = "";
 
 void setup()
 {
-  pinMode(LIGHT_1, OUTPUT);
-  pinMode(LIGHT_2, OUTPUT);
-  digitalWrite(LIGHT_1, HIGH);
-  digitalWrite(LIGHT_2, HIGH);
+
   Serial.begin(115200);
 }
 
@@ -18,23 +13,10 @@ void loop()
   if (Serial.available() > 0)
   {
     DataIn = Serial.readStringUntil('#');
-    if (DataIn == "pOn")
+    if (DataIn == "Command")
     {
-      digitalWrite(LIGHT_1, LOW);
-      delay(5000);
-      digitalWrite(LIGHT_2, LOW);
-    }
-    else if (DataIn == "pOnq")
-    {
-      digitalWrite(LIGHT_1, LOW);
-      delay(500);
-      digitalWrite(LIGHT_2, LOW);
-    }
-    else if (DataIn == "pOff")
-    {
-      digitalWrite(LIGHT_1, HIGH);
-      delay(100);
-      digitalWrite(LIGHT_2, HIGH);
+      // exicute code when cammand is sent
+      delay(1000);
     }
   }
 }
