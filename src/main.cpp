07dd25a91ffca6be7e4488d84af1ad97e068d4e7
jspearus <jspearus@gmail.com>
2022-11-25 08:39:17 -0600

    init

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,8 @@ void setup()
 {
   pinMode(LIGHT_1, OUTPUT);
   pinMode(LIGHT_2, OUTPUT);
+  digitalWrite(LIGHT_1, HIGH);
+  digitalWrite(LIGHT_2, HIGH);
   Serial.begin(115200);
 }
 
@@ -16,5 +18,23 @@ void loop()
   if (Serial.available() > 0)
   {
     DataIn = Serial.readStringUntil('#');
+    if (DataIn == "pOn")
+    {
+      digitalWrite(LIGHT_1, LOW);
+      delay(5000);
+      digitalWrite(LIGHT_2, LOW);
+    }
+    else if (DataIn == "pOnq")
+    {
+      digitalWrite(LIGHT_1, LOW);
+      delay(500);
+      digitalWrite(LIGHT_2, LOW);
+    }
+    else if (DataIn == "pOff")
+    {
+      digitalWrite(LIGHT_1, HIGH);
+      delay(100);
+      digitalWrite(LIGHT_2, HIGH);
+    }
   }
 }
