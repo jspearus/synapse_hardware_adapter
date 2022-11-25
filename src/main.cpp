ca39c1c19293a7a8ba95d5063a2e66eeef686c03
jspearus <jspearus@gmail.com>
2022-11-25 09:01:35 -0600

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include <Arduino.h>
+#include <Servo.h>
+
+Servo monServo; // create servo object to control a servo
+#define servoPin 5
+#define servoDown 80
+#define servoUp 90
 
 String DataIn = "";
 
 void setup()
 {
 
+  monServo.attach(servoPin);
+  monServo.write(servoUp);
   Serial.begin(115200);
 }
 
@@ -13,9 +21,28 @@ void loop()
   if (Serial.available() > 0)
   {
     DataIn = Serial.readStringUntil('#');
-    if (DataIn == "Command")
+    if (DataIn == "mon")
     {
-      // exicute code when cammand is sent
+      // Power On Projector
+      monServo.write(servoUp);
+      delay(100);
+      monServo.write(servoDown);
+      delay(300);
+      monServo.write(servoUp);
+      delay(1000);
+    }
+    else if (DataIn == "moff")
+    {
+      // Power Off Projector
+      monServo.write(servoUp);
+      delay(100);
+      monServo.write(servoDown);
+      delay(200);
+      monServo.write(servoUp);
+      delay(200);
+      monServo.write(servoDown);
+      delay(400);
+      monServo.write(servoUp);
       delay(1000);
     }
   }
