888526ea76a6e2fc484c77138a0b2e973f88a41b
jspearus <jspearus@gmail.com>
2023-12-09 06:48:21 -0600

    update

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@
 
 #define S_LIGHT_1 5 // PWM Ctrl
 #define S_LIGHT_2 6 // PWM Ctrl
-#define S_LIGHT_3 7
-#define S_LIGHT_4 8
+#define S_LIGHT_3 3
+#define S_LIGHT_4 9
 
 String DataIn = "";
 boolean flicker = false; // enable street light fflicker mode
