692360f19d6e5685f628fc85e0b7849a60b96c50
jspearus <35698087+jspearus@users.noreply.github.com>
2024-10-23 14:49:47 -0500

    update

--- src/main.cpp
+++ src/main.cpp
@@ -7,26 +7,25 @@
 #define BUFFER_SIZE 32 // Adjust based on expected message size
 
 // Which pin on the Arduino is connected to the NeoPixels?
-#define PIXEL1 2  // On Trinket or Gemma, suggest changing this to 1
-#define PIXEL2 5  // On Trinket or Gemma, suggest changing this to 1
-#define PIXEL3 6  // On Trinket or Gemma, suggest changing this to 1
-#define PIXEL4 9  // On Trinket or Gemma, suggest changing this to 1
-#define PIXEL5 10 // On Trinket or Gemma, suggest changing this to 1
+#define PIXEL1 17 // 17
+#define PIXEL2 16 // 16
+#define PIXEL3 10 // 10
+#define PIXEL4 14 // 14
+#define PIXEL5 15 // 15
 
-#define OUT1 18 // Assign pin# to Outlets
-#define OUT2 15
-#define OUT3 14
-#define OUT4 16
-#define OUT5 8
-#define OUT6 7
-#define OUT7 4
-#define OUT8 3
+#define OUT1 9 // Assign pin# to Outlets
+#define OUT2 8
+#define OUT3 7
+#define OUT4 6
+#define OUT5 5
+#define OUT6 4
+#define OUT7 3
+#define OUT8 2
 
 // How many NeoPixels are attached to the Arduino?
-#define NUMPIXEL_CENTER 150 // Popular NeoPixel ring size
-#define NUMPIXELS 150       // Popular NeoPixel ring size
+#define NUMPIXELS 150 // Popular NeoPixel ring size
 
-Adafruit_NeoPixel pixels1(NUMPIXEL_CENTER, PIXEL1, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels1(NUMPIXELS, PIXEL1, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel pixels2(NUMPIXELS, PIXEL2, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel pixels3(NUMPIXELS, PIXEL3, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel pixels4(NUMPIXELS, PIXEL4, NEO_GRB + NEO_KHZ800);
@@ -36,7 +35,7 @@ String getValue(String data, char separator, int index);
 
 void receiveEvent(int howMany);
 
-void pixel(int i, int r, int g, int b);
+void pixel1(int i, int r, int g, int b);
 void pixel2(int i, int r, int g, int b);
 void pixel3(int i, int r, int g, int b);
 void pixel4(int i, int r, int g, int b);
@@ -103,10 +102,10 @@ void receiveEvent(int numBytes)
   index = 0;                  // Reset index for the next message
 
   // Print the received text to the Serial Monitor
-  // Serial.println(receivedText);
+  Serial.println(receivedText);
   DataIn = receivedText;
   // /////////////////////////////////////////////////////////////////////
-  if (DataIn == "show")
+  if (DataIn == "\nshow")
   {
     pixels1.show(); // Send the updated pixel colors to the hardware.
     pixels2.show();
@@ -114,7 +113,7 @@ void receiveEvent(int numBytes)
     pixels4.show();
     pixels5.show();
   }
-  else if (DataIn == "clear")
+  else if (DataIn == "\nclear")
   {
     pixels1.clear();
     pixels2.clear();
@@ -134,13 +133,13 @@ void receiveEvent(int numBytes)
   {
     pixels2.show(); // Send the updated pixel colors to the hardware.
   }
-  else if (DataIn == "\nclear2")
+  else if (DataIn == "\nclear3")
   {
     pixels2.clear();
   }
   else if (DataIn == "\nshow3")
   {
-    pixels3.show(); // Send the updated pixel colors to the hardware.
+    pixels1.show(); // Send the updated pixel colors to the hardware.
   }
   else if (DataIn == "\nclear3")
   {
@@ -162,67 +161,68 @@ void receiveEvent(int numBytes)
   {
     pixels5.clear();
   }
-  else if (DataIn == "out1on")
+  else if (DataIn == "\nout1on")
   {
+
     digitalWrite(OUT1, LOW); // sets the digital pin 13 on
   }
-  else if (DataIn == "out1off")
+  else if (DataIn == "\nout1off")
   {
     digitalWrite(OUT1, HIGH); // sets the digital pin 13 on
   }
-  else if (DataIn == "out2on")
+  else if (DataIn == "\nout2on")
   {
     digitalWrite(OUT2, LOW); // sets the digital pin 13 on
   }
-  else if (DataIn == "out2off")
+  else if (DataIn == "\nout2off")
   {
     digitalWrite(OUT2, HIGH); // sets the digital pin 13 on
   }
-  else if (DataIn == "out3on")
+  else if (DataIn == "\nout3on")
   {
     digitalWrite(OUT3, LOW); // sets the digital pin 13 on
   }
-  else if (DataIn == "out3off")
+  else if (DataIn == "\nout3off")
   {
     digitalWrite(OUT3, HIGH); // sets the digital pin 13 on
   }
-  else if (DataIn == "out4on")
+  else if (DataIn == "\nout4on")
   {
     digitalWrite(OUT4, LOW); // sets the digital pin 13 on
   }
-  else if (DataIn == "out4off")
+  else if (DataIn == "\nout4off")
   {
     digitalWrite(OUT4, HIGH); // sets the digital pin 13 on
   }
-  else if (DataIn == "out5on")
+  else if (DataIn == "\nout5on")
   {
     digitalWrite(OUT5, LOW); // sets the digital pin 13 on
   }
-  else if (DataIn == "out5off")
+  else if (DataIn == "\nout5off")
   {
     digitalWrite(OUT5, HIGH); // sets the digital pin 13 on
   }
-  else if (DataIn == "out6on")
+  else if (DataIn == "\nout6on")
   {
     digitalWrite(OUT6, LOW); // sets the digital pin 13 on
   }
-  else if (DataIn == "out6off")
+  else if (DataIn == "\nout6off")
   {
     digitalWrite(OUT6, HIGH); // sets the digital pin 13 on
   }
-  else if (DataIn == "out7on")
+  else if (DataIn == "\nout7on")
   {
     digitalWrite(OUT7, LOW); // sets the digital pin 13 on
   }
-  else if (DataIn == "out7off")
+  else if (DataIn == "\nout7off")
   {
     digitalWrite(OUT7, HIGH); // sets the digital pin 13 on
   }
-  else if (DataIn == "out8on")
+  else if (DataIn == "\nout8on")
   {
     digitalWrite(OUT8, LOW); // sets the digital pin 13 on
   }
-  else if (DataIn == "out8off")
+  else if (DataIn == "\nout8off")
   {
     digitalWrite(OUT8, HIGH); // sets the digital pin 13 on
   }
@@ -230,19 +230,20 @@ void receiveEvent(int numBytes)
   {
     String string = getValue(DataIn, ',', 1);
     String led = getValue(DataIn, ',', 2);
-    String r = getValue(DataIn, ',', 4);
-    String g = getValue(DataIn, ',', 3);
+    String r = getValue(DataIn, ',', 3);
+    String g = getValue(DataIn, ',', 4);
     String b = getValue(DataIn, ',', 5);
+    // Serial.print(string);
     if (string == "0")
     {
+      pixel1(led.toInt(), r.toInt(), g.toInt(), b.toInt());
       pixel2(led.toInt(), r.toInt(), g.toInt(), b.toInt());
       pixel3(led.toInt(), r.toInt(), g.toInt(), b.toInt());
       pixel4(led.toInt(), r.toInt(), g.toInt(), b.toInt());
-      pixel5(led.toInt(), r.toInt(), g.toInt(), b.toInt());
     }
     else if (string == "1")
     {
-      pixel(led.toInt(), r.toInt(), g.toInt(), b.toInt());
+      pixel1(led.toInt(), r.toInt(), g.toInt(), b.toInt());
     }
     else if (string == "2")
     {
@@ -250,6 +251,7 @@ void receiveEvent(int numBytes)
     }
     else if (string == "3")
     {
+
       pixel3(led.toInt(), r.toInt(), g.toInt(), b.toInt());
     }
     else if (string == "4")
@@ -266,7 +268,11 @@ void receiveEvent(int numBytes)
   // ///////////////////////////////////////////////////////////////////
 }
 
-void pixel(int i, int g, int r, int b)
+// void pixel(int i, int g, int r, int b)
+// {
+//   pixels1.setPixelColor(i, pixels1.Color(r, g, b));
+// }
+void pixel1(int i, int r, int g, int b)
 {
   pixels1.setPixelColor(i, pixels1.Color(r, g, b));
 }
@@ -275,6 +281,7 @@ void pixel2(int i, int r, int g, int b)
 {
   pixels2.setPixelColor(i, pixels2.Color(r, g, b));
 }
+
 void pixel3(int i, int r, int g, int b)
 {
   pixels3.setPixelColor(i, pixels1.Color(r, g, b));
@@ -282,11 +289,12 @@ void pixel3(int i, int r, int g, int b)
 
 void pixel4(int i, int r, int g, int b)
 {
-  pixels4.setPixelColor(i, pixels2.Color(r, g, b));
+  pixels4.setPixelColor(i, pixels4.Color(r, g, b));
 }
+
 void pixel5(int i, int r, int g, int b)
 {
-  pixels5.setPixelColor(i, pixels1.Color(r, g, b));
+  pixels5.setPixelColor(i, pixels5.Color(r, g, b));
 }
 
 String getValue(String data, char separator, int index)
